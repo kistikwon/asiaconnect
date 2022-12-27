eea5c64dc3b05ffb066d3ab0bfa3b1c984656717
kistikwon <111107076+kistikwon@users.noreply.github.com>
2022-12-27 02:03:36 +0000

    3

--- 36node/test.py
+++ 36node/test.py
@@ -4,4 +4,19 @@ import sys
 import re
 import operator
 
-print("hello")
+
+ip = sys.argv[1]
+
+print("hello"+ip)
+print(type(ip))
+print(ip[3])
+
+
+print(ip)
+print(ip.split('.'))
+p=ip.split('.')
+print(p[3])
+print(type(p[3]))
+
+if int(p[3]) < 30 :
+    print('xx')
