0af66e199dcddb3d676065deb135f90c381ea553
kistikwon <111107076+kistikwon@users.noreply.github.com>
2023-02-24 10:07:18 +0900

    Update pass.py

--- 36node/pass.py
+++ 36node/pass.py
@@ -1,4 +1,10 @@
 from getnodes import getnodes
 from networkpass import networkpass
 
+    resources={}
+    resourcesNodes={203.250.172.1,203.250.172.2,203.250.172.3}
+    selectedZone="None"
+    selectedNode="None"
 
+    selectedNode=getnodes(resourcesNodes)
+    print(selectedNode)
