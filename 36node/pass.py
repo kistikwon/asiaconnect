8d9856a5c07f0c51e730f2cb0c65e5f48e2ff5c9
kistikwon <111107076+kistikwon@users.noreply.github.com>
2023-02-24 01:51:47 +0000

    ok2

--- 36node/pass.py
+++ 36node/pass.py
@@ -3,7 +3,7 @@ from networkpass import networkpass
 
 # python pass.py 203.250.172.1 203.250.172.2 203.250.172.3 203.250.172.4 2
 # ['pass.py', '203.250.172.1', '203.250.172.2', '203.250.172.3', '203.250.172.4', '2']
-print(sys.argv)
+# print(sys.argv)
 
 #test = networkpass(sys.argv)
 
